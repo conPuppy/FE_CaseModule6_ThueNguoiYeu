bb6126683c4da556812a4a5d201dd4e92a129835
pocytamy <phamvantuan.133.1996@gmail.com>
2023-03-14 15:07:28 +0700

    chot dev tu day

--- src/app/User/show-profile/show-profile.component.ts
+++ src/app/User/show-profile/show-profile.component.ts
@@ -1,14 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Account } from 'src/app/model/Account';
+import { AccountService } from 'src/app/service/account/account.service';
 
 @Component({
   selector: 'app-show-profile',
   templateUrl: './show-profile.component.html',
   styleUrls: ['./show-profile.component.css']
 })
-export class ShowProfileComponent {
-  constructor(private router:Router) {
+export class ShowProfileComponent implements OnInit{
+  constructor(private router:Router,private accountService:AccountService) {
   }
+  user!:Account;
+
+
+  ngOnInit(): void {
+        this.accountService.findById(this.accountService.getAccountToken().id).subscribe(res=>this.user=res)
+    }
+
   logout(){
     localStorage.clear();
     this.router.navigate(['/login'])
