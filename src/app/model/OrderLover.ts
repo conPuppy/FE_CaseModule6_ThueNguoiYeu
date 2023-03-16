eccc0638ac6ab27343664557b1d327a2838b1008
tudao888 <daotu2106@gmail.com>
2023-03-16 09:36:23 +0700

    done bl8

--- src/app/model/OrderLover.ts
+++ src/app/model/OrderLover.ts
@@ -7,18 +7,21 @@ export class OrderLover{
     orderTime: number;
     total: number;
     dateOfOrder: string;
+    startOrder: string;
+    endOrder: string;
 
     account:Account;
     
     provider: Provider;
 
-
-    constructor(id: number, statusOrder: number, orderTime: number, total: number, dateOfOrder: string, account:Account, provider:Provider) {
+    constructor(id: number, statusOrder: number, orderTime: number, total: number, dateOfOrder: string, startOrder: string, endOrder: string, account:Account, provider:Provider) {
         this.id = id;
         this.statusOrder = statusOrder;
         this.orderTime = orderTime;
         this.total = total;
         this.dateOfOrder = dateOfOrder;
+        this.startOrder = startOrder;
+        this.endOrder = endOrder;
         this.account = account;
         this.provider = provider;
     }
